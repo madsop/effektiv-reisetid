be18667fb487276a15ec9d8bb1a915f1a384eba0
Mads Opheim <mads.opheim@gmail.com>
2026-08-05 19:28:39 +0200

    create-react-app

---
+++ src/main/webui/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Reisemåte</h1>
+        <p>
+          tabell her
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React2
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
